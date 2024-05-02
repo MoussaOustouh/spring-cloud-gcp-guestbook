9cc8a6d63a78aa78a3e3fd3c3b4c038530639eba
MoussaOustouh <moussa.oustouh@gmail.com>
2024-05-02 16:08:51 +0000

    5a-pubsub

--- 1-bootstrap/guestbook-frontend/src/main/java/com/example/frontend/FrontendController.java
+++ 1-bootstrap/guestbook-frontend/src/main/java/com/example/frontend/FrontendController.java
@@ -6,12 +6,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.*;
 import java.util.*;
+import org.springframework.cloud.gcp.pubsub.core.*;
 
 @Controller
 @SessionAttributes("name")
 public class FrontendController {
 	@Autowired
 	private GuestbookMessagesClient client;
+
+	@Autowired
+    private PubSubTemplate pubSubTemplate;
 	
 	@Value("${greeting:Hello}")
 	private String greeting;
@@ -31,6 +35,8 @@ public class FrontendController {
 		model.addAttribute("name", name);
 		if (message != null && !message.trim().isEmpty()) {
 			// Post the message to the backend service
+			pubSubTemplate.publish("messages", name + ": " + message);
+
 			GuestbookMessage payload = new GuestbookMessage();
 			payload.setName(name);
 			payload.setMessage(message);
